470cf0f9b05fb77014157b7c6b1f2527695b6497
fds1638 <filipsain@yahooc.om>
2023-01-05 13:48:44 -0500

    run tests from root

--- tests/test_certain_game.py
+++ tests/test_certain_game.py
@@ -14,11 +14,9 @@ class TestCertainGame(unittest.TestCase):
         where we are sure of what both players will do (i.e. bet).
         """
         mock_deal_cards.return_value = 'Q', 'K'
-        g, p1, p2, _ = runner.gamerunner.initialize_game(file="test_config.json")
+        g, p1, p2, _ = runner.gamerunner.initialize_game(file="tests/test_config.json")
         g, p1, p2 = runner.gamerunner.play_a_round(g, p1, p2)
         print('{} has {} and {} has {}\n'.format(p1.name, p1.amount, p2.name, p2.amount))
         self.assertEqual(p1.amount,8)
         self.assertEqual(p2.amount,12)
 
-if __name__ == '__main__':
-    unittest.main()
